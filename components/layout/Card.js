339d067a4618975f506cccdf8289a7c5386c969f
elminhoemmanuel <amodue15@gmail.com>
2022-01-20 03:04:22 -0800

    finished working on functions to fetch data from endpoint, poll endpoint every 20 seonds and validate the returned data

---
+++ components/layout/Card.js
@@ -0,0 +1,18 @@
+import React, {useEffect} from 'react';
+
+const Card = ({ data }) => {
+
+    useEffect(() => {
+        
+        console.log(data);
+
+        return () => {
+        }
+    }, [])
+
+    return <div>
+        {data[0].id}
+    </div>;
+};
+
+export default Card;
